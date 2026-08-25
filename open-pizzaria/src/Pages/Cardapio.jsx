1cd074ff4ec2974a59c0fd25a36e5c5321542e5f
Igorz08 <igoroliveiramacedo12.03.2008@gmail.com>
2026-08-25 14:54:50 -0300

    feat: colocando a rota do cardapio no menu

--- open-pizzaria/src/Pages/Cardapio.jsx
+++ open-pizzaria/src/Pages/Cardapio.jsx
@@ -6,43 +6,61 @@ export default function Cardapio() {
   return (
 
     <>
-        
+
+         <div className="cardapio">
+
             <h2>🍕 Pizzas Tradicionais</h2>
 
-            <h3>Calabresa</h3>
-            <p>Molho de tomate, mussarela, calabresa e cebola.</p>
-            <strong>R$ 39,90</strong>
+            <div className="aba">
+                <h3>Calabresa</h3>
+                <p>Molho de tomate, mussarela, calabresa e cebola.</p>
+                <strong>R$ 39,90</strong>
+            </div>
 
-            <h3>Marguerita</h3>
-            <p>Molho de tomate, mussarela, tomate e manjericão.</p>
-            <strong>R$ 42,90</strong>
+            <div className="aba">
+                <h3>Frango com queijo</h3>
+                <p>Molho de tomate, frango desfiado e mussarela.</p>
+                <strong>R$ 39,90</strong>
+            </div>
 
-            <h3>Frango com Catupiry</h3>
-            <p>Molho de tomate, frango desfiado e catupiry.</p>
-            <strong>R$ 44,90</strong>
+            <div className="aba">
+                <h3>Marguerita</h3>
+                <p>Molho de tomate, mussarela, tomate e manjericão.</p>
+                <strong>R$ 42,90</strong>
+            </div>
 
-            <h3>Portuguesa</h3>
-            <p>Molho de tomate, mussarela, presunto, ovo, cebola e azeitona.</p>
-            <strong>R$ 46,90</strong>
+            <div className="aba">
+                <h3>Frango com Catupiry</h3>
+                <p>Molho de tomate, frango desfiado e catupiry.</p>
+                <strong>R$ 44,90</strong>
+            </div>
 
-            <hr>
+            <div className="aba">
+                <h3>Portuguesa</h3>
+                <p>Molho de tomate, mussarela, presunto, ovo, cebola e azeitona.</p>
+                <strong>R$ 46,90</strong>
+            </div>
 
             <h2>⭐ Pizzas Especiais</h2>
 
-            <h3>Quatro Queijos</h3>
-            <p>Mussarela, parmesão, provolone e catupiry.</p>
-            <strong>R$ 49,90</strong>
+            <div className="aba">
+                <h3>Quatro Queijos</h3>
+                <p>Mussarela, parmesão, provolone e catupiry.</p>
+                <strong>R$ 49,90</strong>
+            </div>
 
-            <h3>Strogonoff de Carne</h3>
-            <p>Molho de tomate, mussarela, strogonoff de carne e batata palha.</p>
-            <strong>R$ 52,90</strong>
+            <div className="aba">
+                <h3>Strogonoff de Carne</h3>
+                <p>Molho de tomate, mussarela, strogonoff de carne e batata palha.</p>
+                <strong>R$ 52,90</strong>
+            </div>
 
-            <h3>Costela BBQ</h3>
-            <p>Mussarela, costela desfiada, molho barbecue e cebola.</p>
-            <strong>R$ 54,90</strong>
-
-            <hr>
-
+            <div className="aba">
+                <h3>Costela BBQ</h3>
+                <p>Mussarela, costela desfiada, molho barbecue e cebola.</p>
+                <strong>R$ 54,90</strong>
+            </div>
+            
             <h2>🥤 Bebidas</h2>
 
             <ul>
@@ -52,47 +70,18 @@ export default function Cardapio() {
                 <li>Água — R$ 4,00</li>
             </ul>
 
-            </hr>
-
-            <h2>🍰 Sobremesas</h2>
-
-            <ul>
-                <li>Pizza de chocolate — R$ 29,90</li>
-                <li>Pizza de banana com canela — R$ 27,90</li>
-                <li>Pudim — R$ 10,00</li>
-            </ul>
-
-            </hr>
-
             <h2>📋 Tamanhos</h2>
 
-            <table border="1">
-                <tr>
-                    <th>Tamanho</th>
-                    <th>Quantidade de fatias</th>
-                </tr>
-
-                <tr>
-                    <td>Pequena</td>
-                    <td>4 fatias</td>
-                </tr>
-
-                <tr>
-                    <td>Média</td>
-                    <td>6 fatias</td>
-                </tr>
-
-                <tr>
-                    <td>Grande</td>
-                    <td>8 fatias</td>
-                </tr>
-
-            </table>
-
-            <br/>
+            <ul>
+                <li>Broto - 4 fatias</li>
+                <li>Média - 8 fatias</li>
+                <li>Grande - 10 fatias</li>
+                <li>Familia - 12 fatias</li>
+            </ul>
 
             <button>Fazer pedido</button>
 
+        </div>
         
     </>
 
